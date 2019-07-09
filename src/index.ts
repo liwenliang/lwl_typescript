a0f6bd3649af05350132abfb4d03a8f8f936f364
liwenliang <792831375@qq.com>
2019-07-09 14:09:07 +0800

    更新代码

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,18 @@
-import Vue from "vue";
-import HelloComponent from "./components/Hello.vue";
-import HelloDecoratorComponent from "./components/HelloDecorator.vue";
+enum Color { Red = 100, Green, Blue }
 
-let v = new Vue({
-    el: "#app",
-    template: `
-    <div>
-        Name: <input v-model="name" type="text">
-        <h1>Hello Component</h1>
-        <hello-component :name="name" :initialEnthusiasm="5" />
-        <h1>Hello Decorator Component</h1>
-        <hello-decorator-component :name="name" :initialEnthusiasm="5" />
-        </div>
-    `,
-    data: { name: "World" },
-    components: {
-        HelloComponent,
-        HelloDecoratorComponent
-    }
-});
+let c: Color = Color.Green
+
+console.log(c);
+
+function warnUser(): number {
+    return 1234
+}
+
+let herName: string = 'smiss';
+
+let age: number = 37;
+let sentence: string = `Hello, my name is ${ herName }.
+
+I'll be ${ age + 1 } years old next month.`;
+
+console.log(sentence);
--- webpack.config.js
+++ webpack.config.js
@@ -12,20 +12,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
-          }
-          // other vue-loader options go here
-        }
-      },
-      {
         test: /\.tsx?$/,
         loader: 'ts-loader',
         exclude: /node_modules/,
@@ -45,7 +31,6 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
     }
   },
   devServer: {
